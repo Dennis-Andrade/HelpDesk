feacc2ea6a8b35ff990650a9819ffac94cf4931b
Dennis-Andrade <denisandradeg@gmail.com>
2025-09-25 14:22:38 -0500

    Delete app directory

--- app/Middleware/Middleware.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Middleware;
-
-interface Middleware {
-  /** @param callable $next */
-  public function handle(callable $next);
-}
--- app/Repositories/UserReaderInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Repositories;
-
-interface UserReaderInterface {
-    /** @return array|null {id, username, email, activo, id_rol, password_md5, nombre_completo, rol_nombre} */
-    public function findByIdentity(string $identity): ?array; // usuario o email
-}
