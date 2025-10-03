6dbb57ff325d8def1a88801420935662e8f6724e
Dennis-Andrade <denisandradeg@gmail.com>
2025-10-03 16:30:29 -0500

    Refine commercial dashboard palette and layout

--- config/routes.php
+++ config/routes.php
@@ -70,9 +70,24 @@ $router->get(
     [ContactosController::class, 'index'],
     ['middleware'=>['auth','role:comercial']]
 );
+$router->get(
+    '/comercial/contactos/sugerencias',
+    [ContactosController::class, 'suggest'],
+    ['middleware'=>['auth','role:comercial']]
+);
 $router->post(
     '/comercial/contactos',
     [ContactosController::class, 'create'],
+    ['middleware'=>['auth','role:comercial']]
+);
+$router->get(
+    '/comercial/contactos/editar',
+    [ContactosController::class, 'editForm'],
+    ['middleware'=>['auth','role:comercial']]
+);
+$router->post(
+    '/comercial/contactos/{id}',
+    [ContactosController::class, 'update'],
     ['middleware'=>['auth','role:comercial']]
 );
 $router->post(
