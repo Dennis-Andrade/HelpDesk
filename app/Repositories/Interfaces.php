f7702cfc1ac22cbaf67a36cf6eaa418c81f4f4e7
Dennis-Andrade <denisandradeg@gmail.com>
2025-10-06 16:50:25 -0500

    Delete app directory

--- app/Repositories/Interfaces.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Repositories;
-
-interface PaginatedSearcher {
-  /** @return array{items: array<int, array>, total: int} */
-  public function search(string $q, int $offset, int $limit): array;
-}
