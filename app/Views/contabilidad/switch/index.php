6c9ea259370aa9443f52f96fe767ea5c885cc764
Dennis-Andrade <denisandradeg@gmail.com>
2025-10-22 14:34:10 -0500

    Delete app directory

--- app/Views/contabilidad/switch/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$crumbs = $crumbs ?? [];
-include __DIR__ . '/../../partials/breadcrumbs.php';
-?>
-<link rel="stylesheet" href="/css/contabilidad.css">
-
-<section class="card ent-container" aria-labelledby="switch-title">
-  <h1 id="switch-title" class="ent-title">Switch</h1>
-</section>
