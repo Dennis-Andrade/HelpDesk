a14b709f403794cea20d3d6265d68e6b0c393e38
Dennis-Andrade <denisandradeg@gmail.com>
2025-10-22 14:38:53 -0500

    Add files via upload

---
+++ app/Views/contabilidad/switch/index.php
@@ -0,0 +1,9 @@
+<?php
+$crumbs = $crumbs ?? [];
+include __DIR__ . '/../../partials/breadcrumbs.php';
+?>
+<link rel="stylesheet" href="/css/contabilidad.css">
+
+<section class="card ent-container" aria-labelledby="switch-title">
+  <h1 id="switch-title" class="ent-title">Switch</h1>
+</section>
